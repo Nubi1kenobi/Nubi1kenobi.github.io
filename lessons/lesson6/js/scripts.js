e9d5bd32c8e197eb0e60d582b2192d84023c6a41
Nubi1kenobi <bryan.stauch@gmail.com>
2021-03-16 16:11:20 -0400

    beginningweek6assignment

--- lessons/lesson6/js/scripts.js
+++ lessons/lesson6/js/scripts.js
@@ -1,3 +1,22 @@
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+function theBanner() 
+{
+  var d = new Date();
+  var n = d.getDay();
+
+  if(n === 5) 
+  {
+    var element = document.getElementById("banner")
+    element.classList.add("show");
+    return;
+  }
+  else
+  { 
+    var element = document.getElementById("banner")
+    element.classList.add("hide");
+    return;
+  }
+}
